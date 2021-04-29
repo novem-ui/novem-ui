3e97f1f8e379bc4f15c8cc3bda093bed33dd4f47
Leonel Vieyra <lvieyra@santandertecnologia.com.ar>
2021-04-29 12:19:19 -0300

    Finish build config

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -3,5 +3,5 @@ export { default as NovemProvider } from './src/novem-provider'
 export { default as Badge } from '@novem-ui/badge'
 export * from '@novem-ui/badge'
 
-export { default as theme, Theme } from '@novem-ui/theme'
-
+export { default as theme } from '@novem-ui/theme'
+export type { Theme } from '@novem-ui/theme'

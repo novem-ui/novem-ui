f29b1b2917b3c96dc78e7658e67134af424cf845
Leonel Vieyra <lvieyra@santandertecnologia.com.ar>
2021-07-05 20:10:23 -0300

    export modules on core

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -3,5 +3,13 @@ export { default as NovemProvider } from './src/novem-provider'
 export { default as Badge } from '@novem-ui/badge'
 export * from '@novem-ui/badge'
 
+export { default as Button, ButtonProps } from '@novem-ui/button'
+
+export { default as Switch, SwitchProps } from '@novem-ui/switch'
+
+export { Heading, Label, Paragraph, HeadingProps, LabelProps, ParagraphProps } from '@novem-ui/text'
+
+export { Input, TextField, TextFieldProps } from '@novem-ui/input'
+
 export { default as theme } from '@novem-ui/theme'
 export type { Theme } from '@novem-ui/theme'

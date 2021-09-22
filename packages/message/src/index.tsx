bf07977bda6a26f9536599eb59c70c93934cc31d
Leonel Vieyra <me@leonelv.com>
2021-09-22 20:51:52 -0300

    refactor(message): use novem components instead of making them from scratch

--- packages/message/src/index.tsx
+++ packages/message/src/index.tsx
@@ -1,36 +1,40 @@
 import React, { FunctionComponent } from 'react'
 import { Close as CloseIcon } from '@icon-park/react'
 import { FeedbackBadge } from '@novem-ui/badge'
+import { Box } from '@novem-ui/layout'
+import { Paragraph } from '@novem-ui/text'
+import Button from '@novem-ui/button'
 
-import { Content, Actions, IconWrapper, Text, Title, ContentWrapper, CloseWrapper, MessageWrapper } from './styles'
+import { Content, Actions, MessageWrapper } from './styles'
 import { MessageTypes } from './types'
 
-const Message: FunctionComponent<MessageTypes> = ({ title, message, onClose, action, baseColor, variant }) => (
-  <MessageWrapper baseColor={baseColor}>
-    <Content>
-      <IconWrapper>
-        <FeedbackBadge variant={variant} />
-      </IconWrapper>
-      <ContentWrapper>
-        {title ? (
-          <>
-            <Title>{message}</Title>
-            <Text withTitle={!!title}>{message}</Text>
-          </>
-        ) : (
-          <Text>{message}</Text>
+const Message: FunctionComponent<MessageTypes> = ({ title, message, onClose, action, baseColor, variant }) => {
+  const hasTitle = !!title
+  const messageSize = hasTitle ? 'xs' : 'sm'
+
+  return (
+    <MessageWrapper baseColor={baseColor}>
+      <Content>
+        <FeedbackBadge variant={variant} mt={4.5} />
+        <Box ml={4}>
+          {!!hasTitle && (
+            <Paragraph weight="bold" size="sm" mb={1}>
+              {title}
+            </Paragraph>
+          )}
+          <Paragraph size={messageSize}>{message}</Paragraph>
+        </Box>
+      </Content>
+      <Actions>
+        {action && action}
+        {onClose && (
+          <Button onClick={onClose}>
+            <CloseIcon />
+          </Button>
         )}
-      </ContentWrapper>
-    </Content>
-    <Actions>
-      {action && action}
-      {onClose && (
-        <CloseWrapper onClick={onClose}>
-          <CloseIcon />
-        </CloseWrapper>
-      )}
-    </Actions>
-  </MessageWrapper>
-)
+      </Actions>
+    </MessageWrapper>
+  )
+}
 
 export default Message
--- packages/message/src/styles.tsx
+++ packages/message/src/styles.tsx
@@ -1,25 +1,27 @@
 import styled from '@emotion/styled'
 import { borderColor, BorderColorProps } from '@novem-ui/base'
-import theme from '@novem-ui/theme'
+import { Flex } from '@novem-ui/layout'
+import type { Theme } from '@novem-ui/theme'
 
 export interface TextTypes {
   withTitle?: boolean
 }
 
 export interface IconWrapperTypes {
-  baseColor: keyof typeof theme.colors.palette
+  baseColor: keyof Theme['colors']['palette']
 }
 
 export const MessageWrapper = styled.div<BorderColorProps>`
   align-items: center;
-  border-radius: 1rem;
+  border-radius: ${({ theme }) => `${theme.spacing[4]}rem`};
   border-style: solid;
   border-width: 1px;
   box-shadow: 0 0.125rem 0.375rem rgba(31, 26, 24, 0.12);
   box-sizing: border-box;
   display: flex;
   justify-content: space-between;
-  padding: 1rem;
+  padding: ${({ theme }) => `${theme.spacing[4]}rem`};
+  height: 4.5rem;
   ${({ theme, baseColor }) => borderColor({ theme, baseColor })};
 `
 
@@ -46,25 +48,19 @@ export const Title = styled.p`
 `
 
 export const IconWrapper = styled.div`
-  margin-top: 1.125rem;
+  margin-top: ${({ theme }) => `${theme.spacing[4.5]}rem`};
 `
 
-export const ContentWrapper = styled.div`
-  display: grid;
-  gap: 0.25rem;
-  margin: 0 0 0 1rem;
-`
-
-export const CloseWrapper = styled.div`
+export const CloseWrapper = styled(Flex)`
   align-items: center;
   cursor: pointer;
   display: flex;
-  height: 0.75rem;
-  margin-left: 1rem;
-  width: 0.75rem;
+  height: ${({ theme }) => `${theme.spacing[3]}rem`};
+  margin-left: ${({ theme }) => `${theme.spacing[4]}rem`};
+  width: ${({ theme }) => `${theme.spacing[3]}rem`};
 
   svg {
-    height: 0.75rem;
-    width: 0.75rem;
+    height: ${({ theme }) => `${theme.spacing[3]}rem`};
+    width: ${({ theme }) => `${theme.spacing[3]}rem`};
   }
 `

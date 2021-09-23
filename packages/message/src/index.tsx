710d73edfad7abadd3415350361969a1dd892ece
Leonel Vieyra <me@leonelv.com>
2021-09-22 21:13:32 -0300

    refactor(message): replace unnecessary custom components with the novem ones

--- packages/message/src/index.tsx
+++ packages/message/src/index.tsx
@@ -1,11 +1,11 @@
 import React, { FunctionComponent } from 'react'
 import { Close as CloseIcon } from '@icon-park/react'
 import { FeedbackBadge } from '@novem-ui/badge'
-import { Box } from '@novem-ui/layout'
+import { Box, Flex } from '@novem-ui/layout'
 import { Paragraph } from '@novem-ui/text'
 import Button from '@novem-ui/button'
 
-import { Content, Actions, MessageWrapper } from './styles'
+import { MessageWrapper } from './styles'
 import { MessageTypes } from './types'
 
 const Message: FunctionComponent<MessageTypes> = ({ title, message, onClose, action, baseColor, variant }) => {
@@ -14,7 +14,7 @@ const Message: FunctionComponent<MessageTypes> = ({ title, message, onClose, act
 
   return (
     <MessageWrapper baseColor={baseColor}>
-      <Content>
+      <Flex align="center">
         <FeedbackBadge variant={variant} mt={4.5} />
         <Box ml={4}>
           {!!hasTitle && (
@@ -24,15 +24,16 @@ const Message: FunctionComponent<MessageTypes> = ({ title, message, onClose, act
           )}
           <Paragraph size={messageSize}>{message}</Paragraph>
         </Box>
-      </Content>
-      <Actions>
+      </Flex>
+      <Flex align="center">
         {action && action}
         {onClose && (
-          <Button onClick={onClose}>
-            <CloseIcon />
+          // TODO: i18n this aria-label
+          <Button onClick={onClose} aria-label="close" hierarchy="link" m="0px" p="4px 0px 0px 0px">
+            <CloseIcon size={16} />
           </Button>
         )}
-      </Actions>
+      </Flex>
     </MessageWrapper>
   )
 }
--- packages/message/src/styles.tsx
+++ packages/message/src/styles.tsx
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled'
 import { borderColor, BorderColorProps } from '@novem-ui/base'
-import { Flex } from '@novem-ui/layout'
 import type { Theme } from '@novem-ui/theme'
 
 export interface TextTypes {
@@ -16,51 +15,11 @@ export const MessageWrapper = styled.div<BorderColorProps>`
   border-radius: ${({ theme }) => `${theme.spacing[4]}rem`};
   border-style: solid;
   border-width: 1px;
-  box-shadow: 0 0.125rem 0.375rem rgba(31, 26, 24, 0.12);
+  box-shadow: ${({ theme }) => `0 ${theme.spacing[0.5]}rem ${theme.spacing[1.5]}rem rgba(31, 26, 24, 0.12)`};
   box-sizing: border-box;
   display: flex;
   justify-content: space-between;
   padding: ${({ theme }) => `${theme.spacing[4]}rem`};
-  height: 4.5rem;
+  height: ${({ theme }) => `${theme.spacing[18]}rem`};
   ${({ theme, baseColor }) => borderColor({ theme, baseColor })};
-`
-
-export const Content = styled.div`
-  align-items: center;
-  display: flex;
-`
-
-export const Actions = styled.div`
-  align-items: center;
-  display: flex;
-`
-
-export const Text = styled.p<TextTypes>`
-  font-size: ${(props) =>
-    props.withTitle ? props.theme.typography.textFontSizes.xs : props.theme.typography.textFontSizes.sm}px;
-  margin: 0;
-`
-
-export const Title = styled.p`
-  font-size: ${(props) => props.theme.typography.textFontSizes.sm}px;
-  font-weight: bold;
-  margin: 0;
-`
-
-export const IconWrapper = styled.div`
-  margin-top: ${({ theme }) => `${theme.spacing[4.5]}rem`};
-`
-
-export const CloseWrapper = styled(Flex)`
-  align-items: center;
-  cursor: pointer;
-  display: flex;
-  height: ${({ theme }) => `${theme.spacing[3]}rem`};
-  margin-left: ${({ theme }) => `${theme.spacing[4]}rem`};
-  width: ${({ theme }) => `${theme.spacing[3]}rem`};
-
-  svg {
-    height: ${({ theme }) => `${theme.spacing[3]}rem`};
-    width: ${({ theme }) => `${theme.spacing[3]}rem`};
-  }
 `

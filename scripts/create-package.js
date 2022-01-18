de88cf3b9cdb166fec9b63bb68198b3db8672bcf
Leonel Vieyra <me@leonelv.com>
2022-01-18 12:29:21 -0300

    chore: fix publish process (#46)
    * fix: verify missing deps and bad imports (#42) (#43)
    
    * fix(button): change withTheme import path in test
    
    * fix(core): add missing packages and exports
    
    * fix(message): add missing badge dep
    
    * fix(text): add missing base dep
    
    * chore: create 0.0.2-alpha.0 release
    
     - @novem-ui/badge@0.0.2-alpha.0
     - @novem-ui/base@0.0.2-alpha.0
     - @novem-ui/button@0.0.2-alpha.0
     - @novem-ui/checkbox@0.0.2-alpha.0
     - @novem-ui/core@0.0.2-alpha.0
     - @novem-ui/input@0.0.2-alpha.0
     - @novem-ui/layout@0.0.2-alpha.0
     - @novem-ui/message@0.0.2-alpha.0
     - @novem-ui/switch@0.0.2-alpha.0
     - @novem-ui/text@0.0.2-alpha.0
     - @types/novem-theme@0.0.2-alpha.0
     - @novem-ui/theme@0.0.2-alpha.0
    
    * fix: set access public for all packages
    
    * chore: copy readme on build
    
    * chore: create 0.0.2-alpha.1 release
    
     - @novem-ui/badge@0.0.2-alpha.1
     - @novem-ui/base@0.0.2-alpha.1
     - @novem-ui/button@0.0.2-alpha.1
     - @novem-ui/checkbox@0.0.2-alpha.1
     - @novem-ui/core@0.0.2-alpha.1
     - @novem-ui/input@0.0.2-alpha.1
     - @novem-ui/layout@0.0.2-alpha.1
     - @novem-ui/message@0.0.2-alpha.1
     - @novem-ui/switch@0.0.2-alpha.1
     - @novem-ui/text@0.0.2-alpha.1
     - @types/novem-theme@0.0.2-alpha.1
     - @novem-ui/theme@0.0.2-alpha.1

--- scripts/create-package.js
+++ scripts/create-package.js
@@ -5,6 +5,7 @@ const path = require('path')
 
 const CWD = process.cwd()
 const packageJsonPath = path.join(CWD, 'package.json')
+const readmePath = path.join(CWD, 'README.md')
 
 const {
   name,
@@ -16,11 +17,12 @@ const {
   repository,
   bugs,
   peerDependencies,
-  dependencies,
+  dependencies
 } = JSON.parse(fs.readFileSync(packageJsonPath))
 
 const folderName = path.basename(CWD)
 const newPackageJsonPath = path.resolve(__dirname, '..', 'dist', folderName, 'package.json')
+const readmeCopyPath = path.resolve(__dirname, '..', 'dist', folderName, 'README.md')
 
 const cleanPackage = {
   name,
@@ -35,6 +37,10 @@ const cleanPackage = {
   dependencies,
   main: './index.js',
   types: './index.d.ts',
+  publishConfig: {
+    access: 'public'
+  }
 }
 
 fs.writeFileSync(newPackageJsonPath, JSON.stringify(cleanPackage, null, 2))
+fs.copyFileSync(readmePath, readmeCopyPath)

b5ea5bf929db953734281536172ae376d0a6bf20
Leonel Vieyra <me@leonelv.com>
2022-01-18 13:49:08 -0300

    feat(layout): add container component (#57)
    * feat(layout): add container component
    
    * feat(layout): change export method

--- packages/layout/README.md
+++ packages/layout/README.md
@@ -42,3 +42,25 @@ function App() {
   )
 }
 ```
+
+### `<Container />`
+
+Provides a set of breakpoints, similar to the ones in other libs like tailwind or bootstrap.
+It's the same as Box but includes [`FlexProps`](../base/src/types.ts) and the following props:
+
+- `fluid` (Optional): A boolean to set the container to fluid.
+
+```tsx
+import { Container, ThemeProvider } from '@novem-ui/core'
+// or
+import { Container } from '@novem-ui/layout'
+import { ThemeProvider } from '@novem-ui/theme'
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Container fluid display="flex" align="center" justify="left" />
+    </ThemeProvider>
+  )
+}
+```
---
+++ packages/layout/src/container/index.ts
@@ -0,0 +1,58 @@
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+import {
+  border,
+  background,
+  layout,
+  spacing,
+  BorderProps,
+  BackgroundProps,
+  LayoutProps,
+  SpacingProps,
+  shadows,
+  ShadowProps,
+  flex,
+  FlexProps
+} from '@novem-ui/base'
+
+export type ContainerProps = Omit<
+  BorderProps & BackgroundProps & LayoutProps & SpacingProps & ShadowProps & FlexProps,
+  'width'
+> & {
+  fluid?: boolean
+}
+
+const breakpoints = css`
+  @media (min-width: 576px) {
+    max-width: 540px;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
+
+  @media (min-width: 992px) {
+    max-width: 960px;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
+
+  @media (min-width: 1400px) {
+    max-width: 1320px;
+  }
+`
+
+export const Container = styled.div<ContainerProps>`
+  box-sizing: border-box;
+  ${border}
+  ${background}
+  ${flex}
+  ${layout}
+  ${spacing}
+  ${shadows}
+  width: 100%;
+
+  ${({ fluid }) => !fluid && breakpoints}
+`

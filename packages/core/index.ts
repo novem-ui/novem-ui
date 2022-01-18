35e101e9f21b860eae25047c74cfc67c6ce304ea
Leonel Vieyra <me@leonelv.com>
2022-01-18 01:11:08 -0300

    fix(button): change withTheme import path in test

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -12,7 +12,7 @@ export { default as Checkbox, CheckboxProps } from '@novem-ui/checkbox'
 
 export { Heading, Label, Paragraph, HeadingProps, LabelProps, ParagraphProps } from '@novem-ui/text'
 
-export { Input, TextField, TextFieldProps } from '@novem-ui/input'
+export { Input, TextField, TextFieldProps, Option, OptionProps, Dropdown, DropdownProps } from '@novem-ui/input'
 
 export { default as theme } from '@novem-ui/theme'
 export type { Theme } from '@novem-ui/theme'

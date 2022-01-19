18bf37ab6e09051eb8e311a08c9f352e26edb56b
Leonel Vieyra <me@leonelv.com>
2022-01-18 22:07:44 -0300

    feat(avatar): add spacing props

--- packages/avatar/src/index.tsx
+++ packages/avatar/src/index.tsx
@@ -2,12 +2,19 @@ import React, { VoidFunctionComponent } from 'react'
 
 import { Flex } from '@novem-ui/layout'
 
+import { SpacingProps } from '@novem-ui/base'
 import AvatarImage from './avatar-image'
 import { AvatarProps } from './types'
 import AvatarText from './avatar-text'
 import { getAvatarSize } from './utils'
 
-const Avatar: VoidFunctionComponent<AvatarProps> = ({ size = 'md', image, name, description }) => {
+const Avatar: VoidFunctionComponent<AvatarProps & SpacingProps> = ({
+  size = 'md',
+  image,
+  name,
+  description,
+  ...props
+}) => {
   if (!image && !name) {
     throw new Error(`Avatar should receive at least an image or a name prop`)
   }
@@ -15,7 +22,7 @@ const Avatar: VoidFunctionComponent<AvatarProps> = ({ size = 'md', image, name, 
   const avatarSize = getAvatarSize(size)
 
   return (
-    <Flex height={avatarSize}>
+    <Flex height={avatarSize} {...props}>
       <AvatarImage image={image} name={name} avatarSize={avatarSize} />
       {name && <AvatarText name={name} size={size} description={description} />}
     </Flex>
